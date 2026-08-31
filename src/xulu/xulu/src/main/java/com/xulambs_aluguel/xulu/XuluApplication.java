0f587af6a76bae1b414216c2e8c06623914856ae
pazzutti <arthur.pazzutti0@gmail.com>
2026-08-31 11:38:36 -0300

    build: estrutura inicial springboot

---
+++ src/xulu/xulu/src/main/java/com/xulambs_aluguel/xulu/XuluApplication.java
@@ -0,0 +1,13 @@
+package com.xulambs_aluguel.xulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XuluApplication.class, args);
+	}
+
+}
---
+++ src/xulu/xulu/src/test/java/com/xulambs_aluguel/xulu/XuluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xulambs_aluguel.xulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
